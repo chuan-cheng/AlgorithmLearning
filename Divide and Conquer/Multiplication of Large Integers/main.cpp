820a5584908014f6d06a91fe2e7740f1b875e309
chuan-cheng <67964365+chuan-cheng@users.noreply.github.com>
2022-03-20 12:54:54 +0800

    Update main.cpp

--- Divide and Conquer/Multiplication of Large Integers/main.cpp
+++ Divide and Conquer/Multiplication of Large Integers/main.cpp
@@ -2,7 +2,7 @@
 #include <cmath>
 
 using namespace std;
-typedef long BigInteger;
+typedef long long BigInteger;
 
 int MaxDigit(BigInteger x, BigInteger y)
 {
@@ -27,19 +27,21 @@ BigInteger Multiply(BigInteger x, BigInteger y)
     int m ;
     BigInteger al, ar, bl, br;
     BigInteger x1, x2, x3;
+
     if (n == 1)
         return x*y;
     else
     {
-        m = floor(n/2);
-        al = x / pow(10, m);
-        ar = x % (int)pow(10, m);
-        bl = y / pow(10, m);
-        br = y % (int)pow(10, m);
-        x1 = Multiply(al,bl);
-        x2 = Multiply(ar,br);
-        x3 = Multiply(al+ar,bl+br);
-        return x1*pow(10, m*2) + (x3-x1-x2)*pow(10,m) + x2;
+        m = floor(n / 2);
+        al = x / (int)pow(10, m);
+        ar = x % (int) pow(10, m);
+        bl = y / (int)pow(10, m);
+        br = y % (int) pow(10, m);
+        x1 = Multiply(al, bl);
+        x2 = Multiply(ar, br);
+        x3 = Multiply(al + ar, bl + br);
+
+        return x1 * (int)pow(10, m * 2) + (x3 - x1 - x2) * (int)pow(10, m) + x2;
     }
 
 }
@@ -47,10 +49,9 @@ BigInteger Multiply(BigInteger x, BigInteger y)
 
 int main()
 {
-        BigInteger a, b, ret;
-        cin >>  a >> b;
+        BigInteger a, b;
+        cin >> a >> b;
 
-        ret = Multiply(a,b);
-        cout<<a<<'x'<<b<<'='<<ret<<endl;
+        cout<<Multiply(a,b)<<endl;
         return 0;
 }
